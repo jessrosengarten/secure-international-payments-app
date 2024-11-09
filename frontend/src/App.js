cf4225a034206e610448b682354246fb1c7f9e53
Rudolf9202 <st10070126@vcconnect.edu.za>
2024-11-09 22:56:10 +0200

    Updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Signup from "./components/signup";
 import Login from "./components/login";
 import CustomerDashboard from "./components/customerDashboard";
@@ -8,19 +8,22 @@ import EmpDashboard from "./components/empDashboard";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    localStorage.removeItem('token');
+  }, []);
   return (
-      <Router>
-        <div className='App'>
-          <Routes>
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/customerDashboard' element={<CustomerDashboard />} />
-            <Route path='/admin' element={<EmpLogin />} />
-            <Route path='/empDashboard' element={<EmpDashboard />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/customerDashboard' element={<CustomerDashboard />} />
+          <Route path='/admin' element={<EmpLogin />} />
+          <Route path='/empDashboard' element={<EmpDashboard />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
